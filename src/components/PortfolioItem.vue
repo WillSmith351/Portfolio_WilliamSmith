d98e01138040d6bd211eb84ba903ddf9be4b519c
WillSmith351 <williamcalvertsmith@gmail.com>
2023-10-28 10:23:30 +0200

    First Commit

---
+++ src/components/PortfolioItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="portfolio-item">
+      <h2>{{ item.title }}</h2>
+      <p>{{ item.description }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'PortfolioItem',
+    props: {
+      item: {
+        type: Object,
+        required: true
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .portfolio-item {
+    margin-bottom: 20px;
+  }
+  </style>
+  
---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="home">
+      <h1>Welcome to My Portfolio</h1>
+      <div class="portfolio-items">
+        <PortfolioItem v-for="item in portfolioItems" :key="item.id" :item="item" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import PortfolioItem from '@/components/PortfolioItem.vue';
+  
+  export default {
+    name: 'HomePage',
+    components: {
+      PortfolioItem
+    },
+    data() {
+      return {
+        portfolioItems: [
+          { id: 1, title: 'Project 1', description: 'Description of project 1.' },
+          { id: 2, title: 'Project 2', description: 'Description of project 2.' },
+          // Ajoute plus d'éléments ici si nécessaire
+        ]
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .home {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .portfolio-items {
+    margin-top: 20px;
+  }
+  </style>
+  
